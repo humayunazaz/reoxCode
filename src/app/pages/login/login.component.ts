d2595958a67af4ff7e0cbc2407f097f8dc688702
maxGoAdmin <admin@maxgo.agency>
2023-10-17 21:25:52 +0500

    the login page is updated

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,8 +3,10 @@ import {
   UntypedFormGroup,
   UntypedFormBuilder,
   Validators,
+  FormControl,
 } from '@angular/forms';
 import { Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -14,6 +16,7 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   public loginForm: UntypedFormGroup;
   public hide = true;
+  public isInternalUser: boolean = false;
   constructor(public fb: UntypedFormBuilder, public router: Router) {}
 
   ngOnInit() {
@@ -28,11 +31,46 @@ export class LoginComponent implements OnInit {
       ],
       rememberMe: false,
     });
+    this.emailListner();
   }
 
   public onLoginFormSubmit(values: Object): void {
     if (this.loginForm.valid) {
       this.router.navigate(['/dashboard']);
+    }
+  }
+
+  public onMsLogin(): void {
+    console.log('implement the MS login');
+  }
+
+  private emailListner(): void {
+    this.loginForm.controls['username'].valueChanges
+      .pipe(debounceTime(400), distinctUntilChanged())
+      .subscribe((enteredEmail: string) => {
+        if (enteredEmail.includes('@stentam.com')) {
+          this.isInternalUser = true;
+          this.internalUserFormHandler(true);
+        } else {
+          if (this.isInternalUser) {
+            this.internalUserFormHandler(false);
+          }
+          this.isInternalUser = false;
+        }
+      });
+  }
+
+  private internalUserFormHandler(showOutlook: boolean): void {
+    if (showOutlook) {
+      this.loginForm.removeControl('password');
+    } else {
+      this.loginForm.addControl(
+        'password',
+        new FormControl(
+          null,
+          Validators.compose([Validators.required, Validators.minLength(6)])
+        )
+      );
     }
   }
 }
